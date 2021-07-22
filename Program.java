f448b5c73f477f9aaad8d8508d956ed7388039b0
Valery Taustsiakou <85172327+valtaust@users.noreply.github.com>
2021-07-22 14:45:24 +0100

    Update Program.java

--- Program.java
+++ Program.java
@@ -3,21 +3,22 @@ package ie.gmit.dip;
 import java.util.Scanner;
 
 public class Program {
-	
+
 	static Scanner sc = new Scanner(System.in);
 	static int num = Integer.valueOf(sc.nextInt());
 
 	public static void greet(int num) {
 		int i = 0;
-	    while (i < num) {
-	        System.out.println("Greetings!");
-	        i++;
-	        break;
-	    }
+		while (i < num) {
+			System.out.println("Greetings!");
+			i++;
+			break;
+		}
 	}
-	
+
 	public static void main(String[] args) {
-	    greet(num);
-	    System.out.println("");
+		greet(num);
+		System.out.println("");
 	}
 }
+
