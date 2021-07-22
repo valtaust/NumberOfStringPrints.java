cd26a68fb70a13745eb43af98b48cc1850eb8646
Valery Taustsiakou <85172327+valtaust@users.noreply.github.com>
2021-07-22 14:44:22 +0100

    i < input. i++ iterates upwards
    i++ then break while num is set by the user.

---
+++ Program.java
@@ -0,0 +1,23 @@
+package ie.gmit.dip;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	static Scanner sc = new Scanner(System.in);
+	static int num = Integer.valueOf(sc.nextInt());
+
+	public static void greet(int num) {
+		int i = 0;
+	    while (i < num) {
+	        System.out.println("Greetings!");
+	        i++;
+	        break;
+	    }
+	}
+	
+	public static void main(String[] args) {
+	    greet(num);
+	    System.out.println("");
+	}
+}
