307bcb63cb2855f422fbca110c2603502f3779ce
Valery Taustsiakou <85172327+valtaust@users.noreply.github.com>
2021-07-22 15:22:03 +0100

    Add files via upload

---
+++ DecrementProgram.java
@@ -0,0 +1,23 @@
+package ie.gmit.dip;
+
+//Program here decrements as expected
+//could also change i to ++ and sysout i instead.
+
+import java.util.Scanner;
+
+public class DecrementProgram {
+
+	static Scanner sc = new Scanner(System.in);
+	static int input = Integer.valueOf(sc.nextInt());
+
+	public static void main(String[] args) {
+		System.out.print("hello is this part working. ");
+		System.out.println("If so print yes");
+
+		int i = 0;
+		while (i < input) {
+			input--;
+			System.out.println(input);
+		}
+	}
+}
